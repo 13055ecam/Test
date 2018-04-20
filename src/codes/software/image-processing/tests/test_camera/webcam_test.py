b36d79d1b405f1176519dd52b80297aa2736d097
Alexandre Lenaerts <13055@ecam.be>
2018-04-20 16:25:49 +0200

    tres

--- src/codes/software/image-processing/tests/test_camera/webcam_test.py
+++ src/codes/software/image-processing/tests/test_camera/webcam_test.py
@@ -1,26 +1,13 @@
-#import RPi.GPIO as GPIO
 import time
 import cv2
-#----------
-
-#GPIO.cleanup()
-
-#GPIO.setmode(GPIO.BCM) 
-#GPIO.setup(4, GPIO.OUT) 
 
 # Create an object called camera and connect the firt camera to the computer
 camera = cv2.VideoCapture(0)
-
-#GPIO.output(4, GPIO.LOW)
-#time.sleep(1)
-#GPIO.output(4, GPIO.HIGH)
 
 
 # Take a photo and save it in the current folder 
 return_value, image = camera.read()
 cv2.imwrite('opencv'+'.png', image)
 
-#GPIO.output(4,GPIO.LOW)
-#GPIO.cleanup()
 
 
